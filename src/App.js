25745a2686ec5e76aeb726a541c859971068586e
Michael Maertens <mikemaer1990@gmail.com>
2021-07-07 12:50:15 +0800

    fix useeffect dependency issue

--- src/App.js
+++ src/App.js
@@ -11,39 +11,41 @@ function App() {
   const [status, setStatus] = useState("all");
   const [sortedTodos, setSortedTodos] = useState([]);
 
-  const handleFilter = () => {
-    switch (status) {
-      case "complete":
-        setSortedTodos(todos.filter((item) => item.done === true));
-        break;
-      case "uncomplete":
-        setSortedTodos(todos.filter((item) => item.done === false));
-        break;
-      default:
-        setSortedTodos(todos);
-        break;
+  useEffect(
+    function handleFilter() {
+      switch (status) {
+        case "complete":
+          setSortedTodos(todos.filter((item) => item.done === true));
+          break;
+        case "uncomplete":
+          setSortedTodos(todos.filter((item) => item.done === false));
+          break;
+        default:
+          setSortedTodos(todos);
+          break;
+      }
+    },
+    [setSortedTodos, todos, status]
+  );
+
+  const getLocalTodos = () => {
+    if (!localStorage.getItem("todos")) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let localTodos = JSON.parse(localStorage.getItem("todos"));
+      setTodos(localTodos);
     }
   };
-
-  // const setLocalTodos = () => {
-  //   localStorage.setItem("todos", JSON.stringify(todos));
-  // };
-
-  // const getLocalTodos = () => {
-  //   if (localStorage.getItem("todos") === null) {
-  //     localStorage.setItem("todos", JSON.stringify([]));
-  //   } else {
-  //     let localTodos = JSON.parse(localStorage.getItem("todos"));
-  //     setTodos(localTodos);
-  //   }
-  // };
-  // useEffect(() => {
-  //   setLocalTodos();
-  // }, []);
   useEffect(() => {
-    handleFilter();
-    // getLocalTodos();
-  }, [todos, status]);
+    getLocalTodos();
+  }, []);
+
+  useEffect(
+    function setLocalTodos() {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    },
+    [todos]
+  );
   // Return app
   return (
     <div className="w-screen h-screen bg-gray-800 pt-24">
